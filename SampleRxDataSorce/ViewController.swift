8a0f8c70890333b348654b7091fb5e7aa1b243ab
jun-morita <jun.morita@gree.net>
2020-08-09 18:38:13 +0900

    Fist Commit

--- SampleRxDataSorce/ViewController.swift
+++ SampleRxDataSorce/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private var tableView: UITableView!
+    private var items = ["TableView", "CollectionView"]
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let indexPathForSelectedRow = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: indexPathForSelectedRow, animated: true)
+        }
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let sb = UIStoryboard(name: "SampleWithTableViewController", bundle: nil)
+            let vc = sb.instantiateViewController(withIdentifier: "SampleWithTableViewController") as! SampleWithTableViewController
+            navigationController?.pushViewController(vc, animated: true)
+        } else if indexPath.row == 1 {
+            
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else {
+            return UITableViewCell()
+        }
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+}
